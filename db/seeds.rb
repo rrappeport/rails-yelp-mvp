e6915e08c777b96c857d0c12aba9663d4c5f0b8a
Ryan Rappeport <rrappeport@gmail.com>
2018-07-28 13:33:34 +0800

    Controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  Restaurant.create(
+    name: Faker::LordOfTheRings.location,
+    address: Faker::Address.street_address,
+    category: "japanese"
+    )
+end
